7ba5f506feb26acddd877f30e1b2df5f6da53d3e
kuyawa <haxapp@gmail.com>
2020-05-21 10:32:30 -0400

    Trade

--- index.php
+++ index.php
@@ -26,6 +26,9 @@ if($price["BTC"] && $price["BTC"] > 0){ $btc = 1/$price["BTC"]; }
 	<meta name="theme-color" content="#0A161C">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 	<link rel="stylesheet" type="text/css" href="static/style.css">
+    <link rel="icon" href="static/mobile-icon.png">
+    <link rel="icon" sizes="192x192" href="static/mobile-icon-192.png">
+    <link rel="apple-touch-icon" href="static/mobile-icon-apple.png"/>
 </head>
 <body>
 
@@ -46,12 +49,12 @@ if($price["BTC"] && $price["BTC"] > 0){ $btc = 1/$price["BTC"]; }
 
 <div id="demo">
 	<wrap>
-		<h1 class="demo-title">HOW TO TRADE FOREX</h1>
+		<h1 class="demo-title">HOW TO TRADE IN STARFOREX</h1>
 		<h2 class="demo-subtitle">It Is Really Simple</h2>
 		<img class="demo-icon" src="static/icon1.png">
 		<li>Before buying world currencies first you have to create a trustline to the asset being exchanged in your wallet. The account issuer for all assets is <span class="bank">GBANKIPYLOQ22HODF7RSQXB6Y3X46USJPSEFTOISSNQZPU6EKCRKACYU</span> and the home domain is starforex.co if you want to search for available assets</li>
 		<img class="demo-icon" src="static/icon2.png">
-		<li>In order to buy currencies just read the QR-code using your wallet, a payment operation will be created to send the XLM amount you wish to spend to the following bank address <span class="bank">GBANKIPYLOQ22HODF7RSQXB6Y3X46USJPSEFTOISSNQZPU6EKCRKACYU</span> The currency code you wish to buy goes in the memo field, the bots will autommatically fill the order at the current price and send the currencies right back to your wallet</li>
+		<li>In order to buy currencies just scan the QR-code using your wallet, a payment operation will be created to send the XLM amount you wish to spend to the following bank address <span class="bank">GBANKIPYLOQ22HODF7RSQXB6Y3X46USJPSEFTOISSNQZPU6EKCRKACYU</span> The currency code you wish to buy goes in the memo field, the bots will autommatically fill the order at the current price and send the currencies right back to your wallet</li>
 		<img class="demo-icon" src="static/icon3.png">
 		<li>Once the assets are in your wallet you can buy products from all countries, send them to other wallets or sell them in any of the Stellar Decentralized Exchanges available, no chargebacks, no limits, worldwide, in just seconds. That's the beauty of a digital bank truly designed for modern times</li>
 		<h2 class="demo-endtitle">All Currencies In Your Wallet</h2>
